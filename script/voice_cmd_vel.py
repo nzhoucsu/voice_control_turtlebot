ee8e3efcdc45bcdda64e820b801f4ab73cb56e54
nzhoucsu <nzhoucsu@gmail.com>
2018-04-12 10:00:33 -0400

    change base move topic

--- script/voice_cmd_vel.py
+++ script/voice_cmd_vel.py
@@ -2,9 +2,18 @@
 
 """
 voice_cmd_vel.py is a simple demo of speech recognition.
-  You can control a mobile base using commands found
-  in the corpus file.
+You can control a mobile base using commands found
+in the corpus file.
 """
+
+'''
+Author: Nuo Zhou
+Date:   04/12/18
+Topic:  Autonomous Intelligent Robotics Assignment 5
+Comments:
+        This paragraph code is edited based on 
+        https://github.com/mikeferguson/pocketsphinx/blob/indigo-devel/nodes/voice_cmd_vel.py
+'''
 
 import roslib; roslib.load_manifest('pocketsphinx')
 import rospy
@@ -21,7 +30,7 @@ class voice_cmd_vel:
         self.msg = Twist()
 
         # publish to cmd_vel, subscribe to speech output
-        self.pub_ = rospy.Publisher('cmd_vel', Twist)
+        self.pub_ = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
         rospy.Subscriber('recognizer/output', String, self.speechCb)
 
         r = rospy.Rate(10.0)
@@ -32,25 +41,30 @@ class voice_cmd_vel:
     def speechCb(self, msg):
         rospy.loginfo(msg.data)
 
-        if msg.data.find("forward") > -1:    
+        if msg.data.find("forward") > -1: 
+            rospy.loginfo("=== input: forward ===")   
             self.msg.linear.x = self.speed
             self.msg.angular.z = 0
         elif msg.data.find("left") > -1:
+            rospy.loginfo("=== input: left ===") 
             if self.msg.linear.x != 0:
                 if self.msg.angular.z < self.speed:
                     self.msg.angular.z += 0.05
             else:        
                 self.msg.angular.z = self.speed*2
-        elif msg.data.find("right") > -1:    
+        elif msg.data.find("right") > -1: 
+            rospy.loginfo("=== input: right ===")    
             if self.msg.linear.x != 0:
                 if self.msg.angular.z > -self.speed:
                     self.msg.angular.z -= 0.05
             else:        
                 self.msg.angular.z = -self.speed*2
         elif msg.data.find("back") > -1:
+            rospy.loginfo("=== input: back ===") 
             self.msg.linear.x = -self.speed
             self.msg.angular.z = 0
-        elif msg.data.find("stop") > -1 or msg.data.find("halt") > -1:          
+        elif msg.data.find("stop") > -1:
+            rospy.loginfo("=== input: stop ===")           
             self.msg = Twist()
         
         self.pub_.publish(self.msg)
